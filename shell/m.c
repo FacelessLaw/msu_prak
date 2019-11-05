c1b97b3567722988db8cb459d82fb68464231b86
Edgar Arutyunov <arutyunov99@mail.ru>
2019-11-05 16:08:42 +0300

    Final result

--- shell/m.c
+++ shell/m.c
@@ -1,116 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+//#include <stdlib.h>
+//#include <string.h>
 
 #include "tools/list/list.h"
 #include "tools/cmdline/cmdline.h"
 
-
-enum {
-    ARG_MAX = 2097152,
-    ARG_CNT = 2,
-    A_FILE_R = 1
-};
-
-char CMD_SEP[10] = " \n";
-
-char *
-readStr(FILE * fr) {
-    int sz = 2;
-    char *s = (char *) realloc(NULL, sizeof(*s) * sz);
-    s[0] = 0;
-    while (fgets(s + strlen(s), sz - strlen(s), fr)) {
-        sz *= 2;
-        s = realloc(s, sz);
-        if (s[strlen(s) - 1] == '\n') {
-            break;
-        }
-    }
-    int len = strlen(s);
-    if (len == 0) {
-        free(s);
-        return NULL;
-    }
-    /*if (s[len - 1] == '\n') {
-        s[len - 1] = 0;
-    }*/
-    return s;
-}
-
-char ** 
-parseWords(char * cmd) {
-    if (!cmd) { 
-        return NULL; 
-    }
-    int sz = 2;
-    int len = 0;
-    char ** res = (char **) realloc(NULL, sizeof(*res) * sz);
-    res[len] = strtok(cmd, CMD_SEP);
-    if (!res[len]) {
-        res[len] = cmd;
-        res[len + 1] = NULL;
-        return res;
-    }
-    ++len;
-    while ((res[len++] = strtok(NULL, CMD_SEP))) {
-        if (len == sz) {
-            sz *= 2;
-            res = (char **) realloc(res, sizeof(*res) * sz);
-        }
-    }
-    return res;
-}
-
-void TEST_LIST() {
-    plist root = NULL;
-    root = add_word(root, "garik");
-    root = add_word(root, "sharik");
-    print_list(root);
-    delete_list(root);
-}
-
 int 
 main(int argc, char *argv[]) {
-    /*if (argc != ARG_CNT) {
-        fprintf(stderr, "I need %d args\n", ARG_CNT);
-        return 1; 
-    }
-    FILE * fr;
-    if (!(fr = fopen(argv[A_FILE_R], "r"))) {
-        fprintf(stderr, "I can't open file %s\n", argv[A_FILE_R]);
-        return 1;
-    }
-    char * str = readStr(fr);
-    fclose(fr);
-    char ** cmdWords = parseWords(str);
-    char ** word = cmdWords;
-    while (word && *word) {
-        printf("%s\n", *(word++));
-    }
-    free(str);
-    free(cmdWords);
-    */
-    
-    
-    /*
-    TEST_LIST();
-    */
-    
-    
-    /*printf("Djarvis ~> ");
-    plist root = NULL;
-    char * word = NULL;
-    int contRead = 1;
-    while (word = read_word(&contRead)) {
-        root = add_word(root, word);
-        free(word);
-        if (!contRead) {
-            break;
-        }
-    }
-    print_list(root);
-    delete_list(root);
-    */
-    parse_cmd();
+    plist cmd = parse_cmd();
+    print_list(cmd);
+    delete_list(cmd);
     return 0;
 }
--- shell/tools/cmdline/cmdline.c
+++ shell/tools/cmdline/cmdline.c
@@ -31,89 +31,10 @@ const char * ERROR_LINE[4] = {
     "\"`\"",
 };
 
-char * read_word(int *endCh) {
-    *endCh = 0;
-    int sz = 2;
-    char * s = (char *) realloc(NULL, sizeof(*s) * sz);
-    s[0] = 0;
-    int len = 0;
-    int ch;
-    while ((ch = getchar()) != EOF && isspace(ch)) {
-        if (ch == '\n') {
-            free(s);
-            *endCh = '\n';
-            return NULL;
-        }
-    }
-    ///While without \'
-    int mode = FREE;
-    int wasReading = 0;
-    while (ch != EOF) {
-        if (mode == FREE) {
-            if (isspace(ch)) {
-                *endCh = ch;
-                break;
-            } else if (ch == '|' || ch == '&' || ch == ';') {
-                
-            }
-            wasReading = 1;
-            if (ch == '\'') {
-                mode = WAIT_ONES;
-            } else if (ch == '\"') {
-                mode = WAIT_PAIR;
-            }else if (ch == '\\') {
-                mode = WAIT_ANY;
-            } else {
-                s = add_ch(s, ch, &len, &sz);
-            }
-        } else if (mode == WAIT_ONES) {
-            if (ch == '\'') {
-                mode = FREE;
-            } else {
-                if (ch == '\n') {
-                    printf("%s", CONT_LINE);
-                }
-                s = add_ch(s, ch, &len, &sz);
-            }
-        } else if (mode == WAIT_PAIR) {
-            /* 
-                тут на самом деле работает еще вставка переменных
-                например $PATH заменяется на значение переменной.
-            */
-            if (ch == '\"') {
-                mode = FREE;
-            } else {
-                if (ch == '\n') {
-                    printf("%s", CONT_LINE);
-                }
-                s = add_ch(s, ch, &len, &sz);
-            }
-        } else if (mode == WAIT_ANY) {
-            if (ch != '\n') {
-                s = add_ch(s, ch, &len, &sz);
-            } else {
-                printf("%s", CONT_LINE);
-            }
-            if (ch != '\\') {
-                mode = FREE;
-            }
-        }
-        ch = getchar();
-    }
-    if (mode != FREE) {
-        fprintf(stderr, "Djarvis: Unexpected EOF during the search %s\n", ERROR_LINE[mode]);
-        fprintf(stderr, "Djarvis: Syntax error \n");
-        free(s);
-        return NULL;
-    }
-    if (wasReading) {
-        return s;
-    }
-    free(s);
-    return NULL;
-}
-
-char * get_cmd() {
+plist parse_cmd() {
+    printf("Djarvis ~> ");
+    
+    plist result = NULL;
     int sz = 2;
     char * s = (char *) realloc(NULL, sizeof(*s) * sz);
     s[0] = 0;
@@ -126,13 +47,17 @@ char * get_cmd() {
     while ((ch = getchar()) != EOF) {
         if (mode == FREE) {
             if (isspace(ch)) {
+                if (!lastWasSpace) {
+                    //s = add_ch(s, ' ', &len, &sz);
+                    result = add_word(result, s);
+                    len = 0;
+                    s[len] = 0;
+                    lastWasSpace = 1;
+                }
                 if (ch == '\n') {
                     break;
                 }
-                if (!lastWasSpace) {
-                    s = add_ch(s, ' ', &len, &sz);
-                    lastWasSpace = 1;
-                }
+                
                 continue;
             }
             wasReading = 1;
@@ -148,7 +73,10 @@ char * get_cmd() {
             } else if (ch == '&') {
                 mode = AMPER;
                 if (!lastWasSpace) {
-                    s = add_ch(s, ' ', &len, &sz);
+                    //s = add_ch(s, ' ', &len, &sz);
+                    result = add_word(result, s);
+                    len = 0;
+                    s[len] = 0;
                 }
                 s = add_ch(s, '&', &len, &sz);
                 lastWasSpace = 0;
@@ -197,30 +125,54 @@ char * get_cmd() {
             }
             if (ch == '&') {
                 s = add_ch(s, '&', &len, &sz);
-                s = add_ch(s, ' ', &len, &sz);
+                result = add_word(result, s);
+                len = 0;
+                s[len] = 0;
+                //s = add_ch(s, ' ', &len, &sz);
                 lastWasSpace = 1;
                 mode = FREE;
             } else if (ch == '\'') {
-                s = add_ch(s, ' ', &len, &sz);
+                result = add_word(result, s);
+                len = 0;
+                s[len] = 0;
+                
+                //s = add_ch(s, ' ', &len, &sz);
                 lastWasSpace = 1;
                 mode = WAIT_ONES;
             } else if (ch == '\"') {
-                s = add_ch(s, ' ', &len, &sz);
+                result = add_word(result, s);
+                len = 0;
+                s[len] = 0;
+                
+                //s = add_ch(s, ' ', &len, &sz);
                 lastWasSpace = 1;
                 mode = WAIT_PAIR;
             } else if (ch == '\\') {
                 mode = WAIT_ANY;
-                s = add_ch(s, ' ', &len, &sz);
+                
+                result = add_word(result, s);
+                len = 0;
+                s[len] = 0;
+                
+                //s = add_ch(s, ' ', &len, &sz);
                 lastWasSpace = 1;
                 mode = FREE;
             } else if (isspace(ch)) {
-                s = add_ch(s, ' ', &len, &sz);
+                result = add_word(result, s);
+                len = 0;
+                s[len] = 0;
+                
+                //s = add_ch(s, ' ', &len, &sz);
                 lastWasSpace = 1;
                 mode = FREE;
             } else {
-                s = add_ch(s, ' ', &len, &sz);
+                result = add_word(result, s);
+                len = 0;
+                s[len] = 0;
+                
+                //s = add_ch(s, ' ', &len, &sz);
                 s = add_ch(s, ch, &len, &sz);
-                lastWasSpace = 1;
+                lastWasSpace = 0;
                 mode = FREE;
             }
         }
@@ -232,20 +184,13 @@ char * get_cmd() {
         return NULL;
     }
     if (wasReading) {
-        return s;
+        if (ch == EOF) { //for files.
+            result = add_word(result, s);
+        }
+        free(s);
+        return result;
     }
     free(s);
+    delete_list(result); // tut result na samom dele doljen byt ravnym NULL
     return NULL;
 }
-
-plist parse_cmd() {
-
-    printf("Djarvis ~> ");
-    
-    char * cmd = get_cmd();
-    //plist res = parse_cmd(cmd);
-    printf("%s\n", cmd);
-    free(cmd);
-    return NULL; 
-}
-
